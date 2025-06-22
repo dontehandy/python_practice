d57f0980dea60fa6dd1a6807d23cfb8267a987a7
Donte Handy <donte.handy@gmail.com>
2025-06-22 18:10:53 -0400

    replace, split at space

--- mini_project_1.py
+++ mini_project_1.py
@@ -14,29 +14,27 @@
 '''
 
 
-unformatted_paragraph = """
-Nestled in the heart of the Pacific Northwest, Washington State is a mesmerizing blend of natural wonders and urban sophistication. 
+unformatted_paragraph = """Nestled in the heart of the Pacific Northwest, Washington State is a mesmerizing blend of natural wonders and urban sophistication. 
 Its geography is a tapestry of contrasts, stretching from the rugged Pacific coastlines to the dense, verdant rainforests of the Olympic Peninsula, culminating in the majestic Cascade Mountains. 
 Among these peaks, Mt. Rainier stands as a sentinel, an active volcano and the pinnacle of the state’s natural beauty. 
 This diverse terrain provides a playground for adventurers and nature lovers alike, offering an array of activities from serene hiking trails and challenging ski slopes to tranquil kayaking routes. 
 Beyond its scenic landscapes, Washington's ecological diversity is a testament to its environmental richness, hosting an array of wildlife that includes the majestic orcas navigating the Puget Sound and elusive elk roaming the shadowy rainforests. 
 This harmonious blend of natural beauty and wildlife creates a unique backdrop for the state, inviting both residents and visitors to immerse themselves in its outdoor wonders. 
-Washington State, through its stunning geography and vibrant ecosystems, stands as a beacon of the Pacific Northwest's splendor, embodying the essence of adventure and the tranquility of nature in one cohesive landscape. 
-"""
+Washington State, through its stunning geography and vibrant ecosystems, stands as a beacon of the Pacific Northwest's splendor, embodying the essence of adventure and the tranquility of nature in one cohesive landscape."""
 
-no_mt = unformatted_paragraph.replace("Mt.", "Mountain")
+replace_mt = unformatted_paragraph.replace("Mt.", "Mountain")
 
-split_to_list = no_mt.split(".")
+split_at_space = replace_mt.split(" ")
 
-print(split_to_list)
-
+print(len(split_at_space)) 
 
 
 
+# i = 0
+# for words in len(split_to_list):
+#     print(words)
 
-
-
-
+    
 
 
 
