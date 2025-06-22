1b0e32969ef65540836ed97ae87bf491da592300
Donte Handy <donte.handy@gmail.com>
2025-06-22 17:36:40 -0400

    Mt. to Mountain and split by period.

--- mini_project_1.py
+++ mini_project_1.py
@@ -2,14 +2,41 @@
 
 # In the paragraph below, write a script to identify the longest sentence by word count.
 
+# answer sheet: 
 '''
+1.  47
+2.  42
+3.  39
+4.  35
+5.  33
+6.  24
+7.  20
+'''
+
+
+unformatted_paragraph = """
 Nestled in the heart of the Pacific Northwest, Washington State is a mesmerizing blend of natural wonders and urban sophistication. 
-Its geography is a tapestry of contrasts, stretching from the rugged Pacific coastlines to the dense, verdant rainforests of the Olympic Peninsula, 
-culminating in the majestic Cascade Mountains. Among these peaks, Mt. Rainier stands as a sentinel, an active volcano and the pinnacle of the state’s natural beauty. 
-This diverse terrain provides a playground for adventurers and nature lovers alike, offering an array of activities from serene hiking trails and challenging ski slopes 
-to tranquil kayaking routes. Beyond its scenic landscapes, Washington's ecological diversity is a testament to its environmental richness, hosting an array of wildlife that 
-includes the majestic orcas navigating the Puget Sound and elusive elk roaming the shadowy rainforests. This harmonious blend of natural beauty and wildlife creates a
-unique backdrop for the state, inviting both residents and visitors to immerse themselves in its outdoor wonders. Washington State, through its stunning geography and vibrant ecosystems, 
-stands as a beacon of the Pacific Northwest's splendor, embodying the essence of adventure and the tranquility of nature in one cohesive landscape.
-'''
+Its geography is a tapestry of contrasts, stretching from the rugged Pacific coastlines to the dense, verdant rainforests of the Olympic Peninsula, culminating in the majestic Cascade Mountains. 
+Among these peaks, Mt. Rainier stands as a sentinel, an active volcano and the pinnacle of the state’s natural beauty. 
+This diverse terrain provides a playground for adventurers and nature lovers alike, offering an array of activities from serene hiking trails and challenging ski slopes to tranquil kayaking routes. 
+Beyond its scenic landscapes, Washington's ecological diversity is a testament to its environmental richness, hosting an array of wildlife that includes the majestic orcas navigating the Puget Sound and elusive elk roaming the shadowy rainforests. 
+This harmonious blend of natural beauty and wildlife creates a unique backdrop for the state, inviting both residents and visitors to immerse themselves in its outdoor wonders. 
+Washington State, through its stunning geography and vibrant ecosystems, stands as a beacon of the Pacific Northwest's splendor, embodying the essence of adventure and the tranquility of nature in one cohesive landscape. 
+"""
+
+no_mt = unformatted_paragraph.replace("Mt.", "Mountain")
+
+split_to_list = no_mt.split(".")
+
+print(split_to_list)
+
+
+
+
+
+
+
+
+
+
 
