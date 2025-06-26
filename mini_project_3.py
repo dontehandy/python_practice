d99c013e48c42df5f4d06ddf0af3b87f606b157b
Donte Handy <donte.handy@gmail.com>
2025-06-26 10:21:06 -0400

    initial commit

---
+++ mini_project_3.py
@@ -0,0 +1,10 @@
+'''
+For this mini project, we are going to build a poker hand evaluator.  For those not familiar with poker, it is a card game played with a standard card deck of 52 cards. 
+
+Poker, a game that comes in various forms, is a card game played with two or more people (the maximum player count is determined by the type of poker game).  The way the cards are dealt can vary wildly, but the base concept is the same:  A player wants to get the best 5-card hand possible and win the chips in the middle that have been bet.
+
+There are many more factors that drive poker gameplay (such as bluffing) but there is always one final comparison that determines a winner of a hand: The showdown.
+
+The showdown is where the players that have not folded compare their hands to each other to find a winner.
+'''
+
