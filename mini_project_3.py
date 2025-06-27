295ccad4de876edc749fce0173d54678b89ae25e
Donte Handy <donte.handy@gmail.com>
2025-06-26 21:13:04 -0400

    two functions

--- mini_project_3.py
+++ mini_project_3.py
@@ -22,20 +22,24 @@ The showdown is where the players that have not folded compare their hands to ea
 #i want to keep everything as and int in the value section 
 
 '''
-1 = "ACE"
 11 = "JACK"
 12 = "QUEEN"
 13 = "KING"
+14 = "ACE"
 '''
 
-card_nums = list(range(1, 14))  # flat list, not a list of a list
+card_nums = list(range(2, 15))  # flat list, not a list of a list
+
+# print(card_nums)
+
 poker_deck = {
     "club": card_nums,
     "diamond": card_nums,
     "heart": card_nums,
     "spade": card_nums
 }
-# print(card_nums)
+
+# print(poker_deck)
 
 total_deck = []
 
@@ -44,6 +48,7 @@ for suit in poker_deck:
         total_deck.append((suit, card))  # add tuple of (suit, value)
 
 # print(total_deck)
+
 #create a hand first
 
 import random
@@ -58,3 +63,34 @@ while i < 5:
         i = i + 1
 
 print(hand)
+
+def highCard():
+
+    if hand[0][0][1] == 14:
+        print("High Card1")
+    if hand[1][0][1] == 14:
+        print("High Card2")
+    if hand[2][0][1] == 14:
+        print("High Card3")
+    if hand[3][0][1] == 14:
+        print("High Card4")
+    if hand[4][0][1] == 14:
+        print("High Card5")
+
+highCard()
+
+
+def onePair():
+
+    if hand[0][0][1] == hand[1][0][1] or  hand[0][0][1] == hand[2][0][1] or hand[0][0][1] == hand[3][0][1] or hand[0][0][1] == hand[4][0][1]:
+        print("one pair1")
+    elif hand[1][0][1] == hand[0][0][1] or  hand[1][0][1] == hand[2][0][1] or hand[1][0][1] == hand[3][0][1] or hand[1][0][1] == hand[4][0][1]:
+        print("one pair2")
+    elif hand[2][0][1] == hand[0][0][1] or  hand[2][0][1] == hand[1][0][1] or hand[2][0][1] == hand[3][0][1] or hand[2][0][1] == hand[4][0][1]:
+        print("one pair3")
+    elif hand[3][0][1] == hand[0][0][1] or  hand[3][0][1] == hand[1][0][1] or hand[3][0][1] == hand[2][0][1] or hand[3][0][1] == hand[4][0][1]:
+        print("one pair4")
+    elif hand[4][0][1] == hand[0][0][1] or  hand[4][0][1] == hand[1][0][1] or hand[4][0][1] == hand[2][0][1] or hand[4][0][1] == hand[3][0][1]:
+        print("one pair5")
+
+onePair()
