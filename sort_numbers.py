98364cc5d234dbc0f8a0a9c7f5c2a26893821b70
Donte Handy <donte.handy@gmail.com>
2025-06-30 18:51:05 -0400

    learned about if nums and if not nums

--- sort_numbers.py
+++ sort_numbers.py
@@ -3,5 +3,18 @@ Finish the solution so that it sorts the passed in array of numbers. If the func
 
 '''
 
-def solution(nums):
-    pass
+def solution(nums):             
+    if not nums:                 # If nums is None or empty #In python, not nums will be True if nums is considered falsy
+        return []                
+    return sorted(nums)          
+
+print(solution([11,33,24]))       
+
+
+def solutionTWO(nums):
+    if nums:
+        return sorted(nums)
+    else:
+        return []
+    
+print(solutionTWO([11,33,24]))
