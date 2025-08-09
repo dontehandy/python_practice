1a349eaee33ba43ce730a021a81e409d74a717e0
Donte Handy <donte.handy@gmail.com>
2025-08-09 12:32:01 -0400

    pom1

--- 09AUG25.py
+++ 09AUG25.py
@@ -1,30 +1,20 @@
-# 1. Peak Element Finder
-def find_peak(arr):
+# 2. Trend Switch Counter
+def count_trend_switches(arr):
     """
-    Returns the index of the first peak in the array.
-    A peak is an element greater than both neighbors.
-    Returns -1 if no peak is found.
+    Counts how many times the array's trend switches
+    from increasing to decreasing or vice versa.
     """
-    for i in range(len(arr)):
-        if arr[i] > arr[-1] and arr[i] > arr[i+1]:
-            return arr.index(arr[i])
-    return -1
-    
+    counter = 0
+    for i in range(1, len(arr)-1):
+        if arr[i] < arr[i-1] and arr[i] > arr[i+1]:
+          counter +=1
+    return counter
 
-print("Testing find_peak...")
-print(find_peak([1, 3, 5, 4, 2]))  # Expected: 2
-print(find_peak([10, 20, 30, 40])) # Expected: -1
-print(find_peak([1, 2, 3, 2, 1]))  # Expected: 2
+print("Testing count_trend_switches...")
+print(count_trend_switches([1, 3, 5, 4, 2, 6, 8, 5]))  # Expected: 2
+print(count_trend_switches([5, 4, 3, 2]))              # Expected: 0
+print(count_trend_switches([1, 2, 1, 2, 1]))           # Expected: 4
 print()
-
-
-# # 2. Trend Switch Counter
-# def count_trend_switches(arr):
-#     """
-#     Counts how many times the array's trend switches
-#     from increasing to decreasing or vice versa.
-#     """
-#     pass
 
 
 # # 3. First Valley Finder
@@ -37,23 +27,29 @@ print()
 #     pass
 
 
-# -----------------
-# TEST CASES
-# -----------------
+# print("Testing find_valley...")
+# print(find_valley([5, 3, 4, 2, 6]))  # Expected: 1
+# print(find_valley([1, 2, 3, 4]))     # Expected: -1
+# print(find_valley([3, 1, 2]))        # Expected: 1
+
+
+# ----completed:
+
+# 1. Peak Element Finder
+# def find_peak(arr):
+#     """
+#     Returns the index of the first peak in the array.
+#     A peak is an element greater than both neighbors.
+#     Returns -1 if no peak is found.
+#     """
+#     for i in range(len(arr)):
+#         if arr[i] > arr[-1] and arr[i] > arr[i+1]:
+#             return arr.index(arr[i])
+#     return -1
+    
 
 # print("Testing find_peak...")
 # print(find_peak([1, 3, 5, 4, 2]))  # Expected: 2
 # print(find_peak([10, 20, 30, 40])) # Expected: -1
 # print(find_peak([1, 2, 3, 2, 1]))  # Expected: 2
 # print()
-
-# print("Testing count_trend_switches...")
-# print(count_trend_switches([1, 3, 5, 4, 2, 6, 8, 5]))  # Expected: 2
-# print(count_trend_switches([5, 4, 3, 2]))              # Expected: 0
-# print(count_trend_switches([1, 2, 1, 2, 1]))           # Expected: 4
-# print()
-
-# print("Testing find_valley...")
-# print(find_valley([5, 3, 4, 2, 6]))  # Expected: 1
-# print(find_valley([1, 2, 3, 4]))     # Expected: -1
-# print(find_valley([3, 1, 2]))        # Expected: 1
