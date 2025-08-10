deb6699b08f017ca096f6a8725adc6dcf684bf0e
Donte Handy <donte.handy@gmail.com>
2025-08-10 12:51:20 -0400

    90% done by me. learn arr 0  and if len else 1, redo

--- 10AUG25.py
+++ 10AUG25.py
@@ -5,7 +5,25 @@ def longest_increasing_streak(arr):
     of consecutive elements in the array.
     If the array is empty, return 0.
     """
-    pass
+    if arr == []:
+        return 0
+    counter = 1
+    length = []
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1]:
+            counter +=1
+        elif arr[i] < arr[i-1]:
+            length.append(counter)
+            counter = 1
+    return max(length) if length else 1
+
+
+
+print("Testing longest_increasing_streak...")
+print(longest_increasing_streak([1, 2, 3, 2, 4, 5, 6, 1]))  # Expected: 4  (2, 4, 5, 6)
+print(longest_increasing_streak([5, 4, 3, 2, 1]))           # Expected: 1  (any single number)
+print(longest_increasing_streak([]))                        # Expected: 0
+print()
 
 
 # 2. Plateau Peak Finder
@@ -35,21 +53,17 @@ def longest_zigzag(arr):
 # TEST CASES
 # -----------------
 
-print("Testing longest_increasing_streak...")
-print(longest_increasing_streak([1, 2, 3, 2, 4, 5, 6, 1]))  # Expected: 4  (2, 4, 5, 6)
-print(longest_increasing_streak([5, 4, 3, 2, 1]))           # Expected: 1  (any single number)
-print(longest_increasing_streak([]))                        # Expected: 0
-print()
 
-print("Testing find_plateau_peak...")
-print(find_plateau_peak([1, 2, 5, 5, 3]))                   # Expected: 2
-print(find_plateau_peak([1, 2, 3, 3, 3, 2, 1]))             # Expected: 2
-print(find_plateau_peak([1, 2, 3, 4, 5]))                   # Expected: -1
-print(find_plateau_peak([5, 5, 4, 3]))                      # Expected: 0
-print()
 
-print("Testing longest_zigzag...")
-print(longest_zigzag([1, 3, 2, 4, 3, 5]))                   # Expected: 6 (whole array zigzags)
-print(longest_zigzag([1, 2, 3, 4]))                         # Expected: 2 (only one up or one down allowed)
-print(longest_zigzag([10]))                                 # Expected: 1
-print(longest_zigzag([1, 3, 3, 2, 4]))                      # Expected: 3 (1, 3, 2 or 3, 2, 4)
+# print("Testing find_plateau_peak...")
+# print(find_plateau_peak([1, 2, 5, 5, 3]))                   # Expected: 2
+# print(find_plateau_peak([1, 2, 3, 3, 3, 2, 1]))             # Expected: 2
+# print(find_plateau_peak([1, 2, 3, 4, 5]))                   # Expected: -1
+# print(find_plateau_peak([5, 5, 4, 3]))                      # Expected: 0
+# print()
+
+# print("Testing longest_zigzag...")
+# print(longest_zigzag([1, 3, 2, 4, 3, 5]))                   # Expected: 6 (whole array zigzags)
+# print(longest_zigzag([1, 2, 3, 4]))                         # Expected: 2 (only one up or one down allowed)
+# print(longest_zigzag([10]))                                 # Expected: 1
+# print(longest_zigzag([1, 3, 3, 2, 4]))                      # Expected: 3 (1, 3, 2 or 3, 2, 4)
