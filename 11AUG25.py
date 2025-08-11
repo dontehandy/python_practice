0c97eb93fc64036a72b78e64b03cb06c5f860bb3
Donte Handy <donte.handy@gmail.com>
2025-08-11 14:48:33 -0400

    done with one

--- 11AUG25.py
+++ 11AUG25.py
@@ -1,46 +1,53 @@
-# 1. Additive Persistence
-def additive_persistence(num):
+# # 1. Additive Persistence
+# def additive_persistence(num):
+#     """
+#     Returns the number of times you must sum the digits of num
+#     until you reach a single digit.
+#     Example: 1679583 -> 1+6+7+9+5+8+3 = 39 -> 3+9 = 12 -> 1+2 = 3 -> output 3
+#     """
+#     counter = 0
+#     while num > 9:
+#         numbers = []
+#         for i in str(num):
+#             numbers.append(int(i))
+#         total = 0
+#         for n in numbers:
+#             total += n
+#         num = total
+#         counter += 1
+#     return counter
+  
+# print("Testing additive_persistence...")
+# print(additive_persistence(1679583))  # Expected: 3
+# print(additive_persistence(1234))     # Expected: 2
+# print(additive_persistence(9))        # Expected: 0
+# print()
+
+
+# 2. Digital Root
+def digital_root(num):
     """
-    Returns the number of times you must sum the digits of num
-    until you reach a single digit.
-    Example: 1679583 -> 1+6+7+9+5+8+3 = 39 -> 3+9 = 12 -> 1+2 = 3 -> output 3
+    Returns the digital root of num.
+    The digital root is the final single-digit value obtained by summing
+    the digits repeatedly.
+    Example: 942 -> 9+4+2 = 15 -> 1+5 = 6
     """
-    the_list = []
-    to_add = []
-    for i in str(num):
-        the_list.append(i)
-    for n in the_list:
-        to_add.append(int(n))
-    return sum(to_add)
-    
-        
-    
+    pass
 
-print("Testing additive_persistence...")
-print(additive_persistence(1679583))  # Expected: 3
-print(additive_persistence(1234))     # Expected: 2
-print(additive_persistence(9))        # Expected: 0
-print()
+# print("Testing digital_root...")
+# print(digital_root(942))               # Expected: 6
+# print(digital_root(132189))            # Expected: 6
+# print(digital_root(493193))            # Expected: 2
+# print()
 
 
-# # 2. Digital Root
-# def digital_root(num):
-#     """
-#     Returns the digital root of num.
-#     The digital root is the final single-digit value obtained by summing
-#     the digits repeatedly.
-#     Example: 942 -> 9+4+2 = 15 -> 1+5 = 6
-#     """
-#     pass
-
-
-# # 3. Factorial Digit Sum
-# def factorial_digit_sum(num):
-#     """
-#     Returns the sum of the factorial of each digit in num.
-#     Example: 145 -> 1! + 4! + 5! = 1 + 24 + 120 = 145
-#     """
-#     pass
+# 3. Factorial Digit Sum
+def factorial_digit_sum(num):
+    """
+    Returns the sum of the factorial of each digit in num.
+    Example: 145 -> 1! + 4! + 5! = 1 + 24 + 120 = 145
+    """
+    pass
 
 
 # -----------------
@@ -49,11 +56,7 @@ print()
 
 
 
-# print("Testing digital_root...")
-# print(digital_root(942))               # Expected: 6
-# print(digital_root(132189))            # Expected: 6
-# print(digital_root(493193))            # Expected: 2
-# print()
+
 
 # print("Testing factorial_digit_sum...")
 # print(factorial_digit_sum(145))        # Expected: 145
